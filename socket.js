c845039f6e6cf138ce162cac16ea2ee973a03715
Thom Nichols <tmnichols@gmail.com>
2013-02-01 11:10:26 -0500

    fixed socket init

--- socket.js
+++ socket.js
@@ -1,12 +1,12 @@
 var io = require('socket.io')
 
 module.exports.listen = function(server) {
-	io.listen(server)
+	io_client = io.listen(server)
 	
-	io.sockets.on('connection', function (socket) {
-		socket.emit('news', { hello: 'world' });
+	io_client.sockets.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' })
 		socket.on('my other event', function (data) {
-			console.log(data);
-		});
-	});
+			console.log(data)
+		})
+	})
 }
