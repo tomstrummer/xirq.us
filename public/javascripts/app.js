cc3d5662c26e8622bc3db5947e173a7a98cb2afe
Thom Nichols <tmnichols@gmail.com>
2013-02-02 18:34:51 -0500

    location search is mostly working

---
+++ public/javascripts/app.js
@@ -0,0 +1,103 @@
+var app = {
+	/**
+	 * Node builder.  Call like:
+	 * n('div',{class:'top'},'inner text') // or:
+	 * n('div',{class:'top'},[n('p',{},'nested element'])
+	 */
+	n: function(e,attrs,inner) {
+		if(typeof(e)=='string') e = document.createElement(e);
+		if (attrs) for (var k in attrs) e.setAttribute(k,attrs[k]);
+		if (inner) {
+			if (typeof(inner)=='string') e.textContent = inner;
+			else if (inner.call) inner.call(e);
+			else for (var i in inner) e.appendChild(inner[i]);
+		}
+		return $(e);
+	},
+
+	/**
+	 * Tweet tmpl:
+	 * http://mir.aculo.us/2011/03/09/little-helpers-a-tweet-sized-javascript-templating-engine/
+	 * Call it like this:
+	 * t("Hello {who}!", { who: "JavaScript" });
+	 */ 
+	t: function(s,d) {
+		for(var p in d)
+			s=s.replace(new RegExp('{'+p+'}','g'), d[p]);
+		return s;
+	},
+
+	relativeTime : function( from ) {
+		var MIN = 60;
+		var HOUR = MIN * 60;
+		var DAY = HOUR * 24;
+		var WEEK = DAY * 7;
+		var MONTH = DAY * 30;
+		var YEAR = DAY * 365;
+		if (typeof(from) == 'string') from = new Date(from).getTime();
+		else if (typeof(from) == 'object') from = from.getTime();
+		var diff = (new Date().getTime() - from)/1000;
+		var pastPresent = ' ago';
+		if ( diff < 0 ) {
+			pastPresent = ' from now';
+			diff = Math.abs(diff);
+		}
+		if ( diff < 1 ) return "just now";
+		if ( diff < MIN ) 
+			var amount = diff, unit = ' second';
+		else if ( diff < HOUR ) 
+			var amount = diff/MIN, unit = ' minute';
+		else if ( diff < DAY ) 
+			var amount = diff/HOUR, unit = ' hour';
+		else if ( diff < WEEK ) 
+			var amount = diff/DAY, unit = ' day';
+		else if ( diff < MONTH ) 
+			var amount = diff/WEEK, unit = ' week';
+		else if ( diff < YEAR ) 
+			var amount = diff/MONTH, unit = ' month';
+		else 
+			var amount = diff/YEAR, unit = ' year';
+		return "" + Math.floor(amount) + unit + (amount < 2 ? "" : "s") + pastPresent;
+	},
+
+	// make text look like it's being typed on the screen :)
+	typewriter : function(dest, text, currentChar, delay ) {
+		dest.html(text.substr(0, currentChar++));
+		if (currentChar>text.length) return; // we are done
+		else setTimeout(function(){
+			app.typewriter(dest,text,currentChar,delay);
+		}, delay);
+	},
+
+	scrollTo : function(selector) {
+		var offset = $(selector).offset();
+		$('html, body').animate({
+				scrollTop: offset.top-30,
+				scrollLeft: offset.left-20
+		});
+	},
+}
+
+if ( typeof(Function.prototype.partial) == "undefined" ) {
+	Function.prototype.partial = function() {
+    var fn = this, args = Array.prototype.slice.call(arguments);
+    return function(){
+      var arg = 0;
+      for ( var i = 0; i < args.length && arg < arguments.length; i++ )
+        if ( args[i] === undefined )
+          args[i] = arguments[arg++];
+      return fn.apply(this, args);
+    };
+  };
+}
+
+// Stub for conole.log for browsers that don't have support
+if ( ! window.console ) {
+	var methods = "debug,error,exception,info,log,trace,warn".split(","),
+	    console = {},
+	    l = methods.length,
+	    fn = function() {}
+
+	while (l--) console[methods[l]] = fn
+	window.console = console
+}
--- public/javascripts/xirqus.js
+++ public/javascripts/xirqus.js
@@ -1,5 +1,6 @@
 var xirqus = (function() {
 	exports = {}
+	exports.map_symbols = {}
 
 	exports.init = function() {
 		xirqus.map = new google.maps.Map( $("#map")[0], {
@@ -8,18 +9,81 @@ var xirqus = (function() {
 			zoom: xirqus.get_last_zoom(),
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		})
+		xirqus.places = new google.maps.places.PlacesService(xirqus.map)
 
-		google.maps.event.addListener(xirqus.map,'center_changed',
+		google.maps.event.addListener( xirqus.map,'center_changed',
 			xirqus.map_center_changed)
-		google.maps.event.addListener(xirqus.map,'zoom_changed',
-			xirqus.map_zoom_changed)
+		google.maps.event.addListener( xirqus.map,'zoom_changed',
+			xirqus.map_zoom_changed )
+		google.maps.event.addListener( xirqus.map,'click',function(evt){
+			setTimeout(xirqus.map_clicked, 300, xirqus.map.zoom, evt)
+		})
 
 		$(window).on('resize orientationChanged', xirqus.adjust_map_bounds)
 		xirqus.adjust_map_bounds()
 
 		$('#my_location').on('click',xirqus.get_my_location)
+		$('#search').on('submit',xirqus.location_search)
 
 		if ( ! localStorage ) localStorage = {}
+	}
+
+	exports.map_clicked = function(zoom,evt) {
+		if ( xirqus.map.zoom != zoom ) {
+			console.log('double click!!')
+			return // double-clicked!
+		}
+		if ( xirqus.map_window ) xirqus.map_window.close()
+
+		xirqus.map_window = new google.maps.InfoWindow({
+			position : evt.latLng,
+			content : $('#searchWindow')[0]
+		})
+		xirqus.map_window.open(xirqus.map)
+
+		$('#searchWindow .searching').show()
+
+		xirqus.places.nearbySearch(
+				{ location : evt.latLng, 
+					radius: 200 }, 
+				xirqus.places_result )
+
+		$('#places').show()
+	}
+
+	exports.places_result = function(places,stat) {
+		$('#searchWindow .searching').hide()
+		var resultList = $('#seachResultList')
+		resultList.text("")
+
+		var resultCodes = google.maps.places.PlacesServiceStatus;
+		if (stat != resultCodes.OK) {
+			console.log('Places search error!', stat, places)
+			if ( stat == resultCodes.ZERO_RESULTS )
+				resultList.append( ich.searchResultItem( { 
+					icon : "about:blank", name:'No results found' } ) )
+			return
+		}
+//		console.log(places)
+
+		for ( i in places ) {
+			var place = places[i]
+			console.log( place )
+			resultList.append( ich.searchResultItem( place ) )
+			if ( i > 6 ) break
+		}
+		xirqus.map_window.setContent($('#searchWindow')[0])
+	}
+
+	exports.location_search = function(evt) {
+		evt.preventDefault()
+		$('#searchWindow .searching').show()
+
+		xirqus.places.nearbySearch(
+			{ location : xirqus.map_window.position, 
+				name : $('#searchBox').val(),
+				radius: 200 }, 
+			xirqus.places_result )
 	}
 
 	exports.map_center_changed = function() {
@@ -44,7 +108,7 @@ var xirqus = (function() {
 	}
 
 	exports.get_last_zoom = function() {
-		return localStorage['last_loc.z'] || 8
+		return parseInt(localStorage['last_loc.z']) || 8
 	}
 
 	exports.get_my_location = function() {
@@ -118,21 +182,22 @@ var xirqus = (function() {
 
 	exports.get_location = function() {
 		if (typeof(navigator.geolocation) == 'undefined') {
-			console.warn('No location');
-			return;
+			console.warn('No location')
+			return
 		}
 		navigator.geolocation.getCurrentPosition(function(pos) {
-				console.debug('location callback!',pos);
+				console.debug('location callback!',pos)
 				position = new google.maps.LatLng(
 					pos.coords.latitude, 
 					pos.coords.longitude);
-				localStorage['last_loc.lat'] = position.lat();
-				localStorage['last_loc.lng'] = position.lng();
+				localStorage['last_loc.lat'] = position.lat()
+				localStorage['last_loc.lng'] = position.lng()
 
+				var symbols = xirqus.map_symbols 
 				if ( pos.coords.accuracy ) { // draw accuracy circle:
-					if ( ! page.myLocAccuracy ) {
-						page.myLocAccuracy = new google.maps.Circle({
-								map: page.map,
+					if ( ! symbols.myLocAccuracy ) {
+						symbols.myLocAccuracy = new google.maps.Circle({
+								map: xirqus.map,
 								center: position,
 								radius: pos.coords.accuracy,
 								strokeColor: "#0081c6",
@@ -140,17 +205,17 @@ var xirqus = (function() {
 								strokeWeight: 2,
 								fillColor: "#0081c6",
 								fillOpacity: 0.35
-							});
+							})
 					}
 					else {
-						page.myLocAccuracy.setCenter(position);
-						page.myLocAccuracy.setRadius(pos.coords.accuracy);
+						symbols.myLocAccuracy.setCenter(position)
+						symbols.myLocAccuracy.setRadius(pos.coords.accuracy)
 					}
 				}
-				if ( ! page.myLocMarker ) {
-					page.myLocMarker = new google.maps.Marker({
+				if ( ! symbols.myLocMarker ) {
+					symbols.myLocMarker = new google.maps.Marker({
 						position: position, 
-						map: page.map, 
+						map: symbols.map, 
 						icon: new google.maps.MarkerImage(
 							'/static/img/my-loc.png',
 							new google.maps.Size(19,19),
@@ -158,20 +223,20 @@ var xirqus = (function() {
 							new google.maps.Point(10,10)
 							),
 						animation: google.maps.Animation.DROP,
-						title:"My location" });
+						title:"My location" })
 				}
-				else page.myLocMarker.setPosition(position);
+				else symbols.myLocMarker.setPosition(position)
 
-				page.map.panTo(position);
+				xirqus.map.panTo(position)
 
-				console.debug('location done.');
+				console.debug('location done.')
 			}, 
 			function(err) {
-				console.log('location error! Code: ' + err.code);
-				app.showMsg('msg_location_err');
-			});
-		console.debug('Asking for location...');
-	};
+				console.log('location error! Code: ' + err.code)
+				app.showMsg('msg_location_err')
+			})
+		console.debug('Asking for location...')
+	}
 
 	return exports
 })()
