71b5ac8771f6bfb22acf6326371665a7f349c962
Thom Nichols <tmnichols@gmail.com>
2013-02-22 12:13:37 -0500

    fixed sample_keys.js

--- sample_keys.js
+++ sample_keys.js
@@ -1,8 +1,9 @@
 // sample keys that will get pulled into the app:
 // rename it to keys.js and *don't check it in!*
 
-module.exports {
+module.exports = {
 	maps_api_key : "your api key here",
-	embedly_key : "embedly key"
-	fb_app_id : "fb app id"
+	embedly_key : "embedly key",
+	fb_app_id : "fb app id",
+	session_secret : "session secret key"
 }
